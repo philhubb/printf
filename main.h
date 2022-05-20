58851e7ce2a20bbcaccaebaa339711ba28abc507
Oyinhoney <shukurah4254@gmail.com>
2022-05-20 21:05:05 +0100

    main.h

--- main.h
+++ main.h
@@ -10,36 +10,20 @@
  * @type: char to compare
  * @f: function to print
  */
+
 typedef struct print
 {
-	char *t;
-	int (*f)(va_list);
-} print_t;
+	char type;
+	int (*funct)(va_list);
+
+} print_f;
 
 int _printf(const char *format, ...);
 int _putchar(char c);
-int print_c(va_list c);
-int print_s(va_list s);
-int print_i(va_list i);
-int print_d(va_list d);
-int print_f(va_list f);
-int print_e(va_list e);
-int print_g(va_list g);
-int print_l(va_list l);
-int print_0(va_list zero);
-int print_h(va_list h);
-int print_u(va_list u);
-int print_b(va_list b);
-int print_o(va_list o);
-int print_x(va_list x);
-int print_X(va_list X);
-int print_p(va_list p);
-int print_S(va_list S);
-int print_r(va_list r);
-int print_R(va_list R);
-int print_add(va_list add);
-int print_less(va_list less);
-int print_space(va_list space);
-int print_sharp(va_list sharp);
+int printNaN(char c1, char c2);
 
+int printchar(va_list a_list);
+int printstr(va_list a_list);
+int printint(va_list a_list);
+int (*_printfunc(char fi))(va_list);
 #endif
